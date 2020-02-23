85e774f2456850673b748caaf9ce5fb6f42603f0
Milo46 <56130120+Milo46@users.noreply.github.com>
2020-02-23 11:30:25 +0100

    Update main.cpp

--- BubbleSort/main.cpp
+++ BubbleSort/main.cpp
@@ -1,1 +1,29 @@
+#include <iostream> //Biblioteka wejscia / wyjscia
+#include <algorithm> //Potrzebna tylko do std::swap();
 
+using namespace std;
+
+int main()
+{
+    int tab[10] = {6, 4, 9, 7, 0, 1, 3, 2, 8, 5}; //Nieposortowana tablica
+    int tab_size = 10; //Rozmiar tablicy
+    
+    //Sortowanie babelkowe
+    for (int i = 0; i < tab_size - 1; i++)
+    {
+        for (int j = 0; j < tab_size - i - 1; j++)
+        {
+            if (tab[j] > tab[j + 1])
+            {
+                swap(tab[j], tab[j + 1]);
+            }
+        }
+    }
+    
+    //Petla wyswietlajace zawartosc tablicy
+    for (int i = 0; i < tab_size; i++) {
+        cout << tab[i] << endl;
+    }
+    
+    //return 0; - niepotrzebne
+}
